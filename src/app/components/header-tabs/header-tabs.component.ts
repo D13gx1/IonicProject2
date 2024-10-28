91099731fe802f1136f67be56edde54059c2678c
D13gx1 <136347264+D13gx1@users.noreply.github.com>
2024-10-27 22:21:31 -0300

    COMMIT 5

---
+++ src/app/components/header-tabs/header-tabs.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { arrowBackOutline, logOut, logOutOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from 'src/app/services/auth.service';
+@Component({
+  selector: 'app-header-tabs',
+  templateUrl: './header-tabs.component.html',
+  styleUrls: ['./header-tabs.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule    // CGV-Permite usar directivas comunes de Angular
+  , FormsModule     // CGV-Permite usar formularios
+  , IonicModule     // CGV-Permite usar componentes de Ionic como IonContent, IonItem, etc.
+  , TranslateModule // CGV-Permite usar pipe 'translate'
+]
+})
+export class HeaderTabsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
